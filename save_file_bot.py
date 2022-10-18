9f42fe31ba7a91f138f8e5d2d52ba03a841e343b
Potfolio205513 <dca.nikita@gmail.com>
2022-10-18 14:56:03 +0400

    some refactor message_worker, add savings datas in json format

--- save_file_bot.py
+++ save_file_bot.py
@@ -4,10 +4,6 @@ from time import sleep
 import json
 
 
-with open("tokens.json", "r") as file:
-    token = json.load(file)["save_file_bot"]
-
-
 class State(object):
     __States = {0: 'commands', 1: 'main_menu', 2: 'save', 3: 'download', 4: 'search', 5: 'file_manager', 6: 'delete',
                 7: 'delete_q', 8: 'rename', 9: 'edit', }
@@ -56,25 +52,26 @@ State.DeleteQ = State(7)
 State.Rename = State(8)
 State.Edit = State(9)
 
+
+with open("tokens.json", "r") as file:
+    token = json.load(file)["save_file_bot"]
+file.close()
+
 bot = telebot.TeleBot(token)
-
-
-def save():
-    temp = open('data_base.txt', 'w', encoding='utf8')
-    temp.write(f'file_system_data = {str(file_system_data)}\n'
-               f'current_file = {str(current_file)}')
-    temp.close()
-
-
-file_system_data = {}
-current_file = {}
-for line in open("data_base.txt", "r", encoding="utf8").read().splitlines():
-    exec(line)
 users = [576109089, 1055941724, 5346695571]
 file_system_default = {'main': ['', 'home', ], 'home': ['main', ]}
 state = State(0)
-message_worker_mode = True
-m_w_file_system_mode = False
+with open("data_base.json", "r") as file:
+    dict_ = json.load(file)
+    file_system_data = dict_["file_system_data"]
+    current_file = dict_["current_file"]
+file.close()
+
+
+def save():
+    with open("data_base.json", "w") as file_:
+        json.dump({"file_system_data": file_system_data, "current_file": current_file}, file_, indent=4, ensure_ascii=False)
+    file_.close()
 
 
 def find_new_name(name, user_id):
@@ -173,6 +170,108 @@ def send_keyboard(message):
                          reply_markup=markup)
 
 
+def main_menu(message):
+    user_id = message.from_user.id
+    global state
+
+    if message.text == 'Загрузить файл':
+        bot.send_message(user_id, 'Этого не умею, но может скоро научусь.')
+    elif message.text == 'Сохранить файл':
+        bot.send_message(user_id, 'Ой, я так не умею, спроси что-нибудь другое.')
+    elif message.text == 'Искать файл':
+        bot.send_message(user_id, 'Ой, этого я пока не умею.')
+    elif message.text == 'Файловая система':
+        state = State.FileManager
+        send_keyboard(message)
+
+
+def file_manager(message):
+    user_id = message.from_user.id
+    global state
+
+    if message.text == 'delete':
+        if current_file[user_id] != 'main':
+            state = State.DeleteQ
+            send_keyboard(message)
+        else:
+            bot.send_message(user_id, 'Нельзя удалять папку: "Мой файловый менеджер".')
+    elif message.text == 'rename':
+        if current_file[user_id] != 'main':
+            state = State.Rename
+            send_keyboard(message)
+        else:
+            bot.send_message(user_id, 'Нельзя переименовывать папку: "Мой файловый менеджер".')
+    elif message.text == 'create':
+        state = State.Edit
+        send_keyboard(message)
+    elif message.text == 'back':
+        if file_system_data[user_id][current_file[user_id]][0] != '':
+            current_file[user_id] = file_system_data[user_id][current_file[user_id]][0]
+            send_keyboard(message)
+        else:
+            bot.send_message(user_id, 'Ты и так уже в самомом начале')
+    elif message.text in file_system_data[user_id]:
+        current_file[user_id] = message.text
+        send_keyboard(message)
+    else:
+        bot.send_message(user_id, 'Такого не знаю попробуй ещё раз')
+        send_keyboard(message)
+
+
+def edit(message):
+    user_id = message.from_user.id
+    global state
+
+    if message.text not in file_system_data[user_id]:
+        file_system_data[user_id][message.text] = [current_file[user_id], ]
+        file_system_data[user_id][current_file[user_id]].append(message.text)
+        current_file[user_id] = message.text
+        bot.send_message(user_id, 'Новая папка создана.')
+        save()
+        state = State.FileManager
+        send_keyboard(message)
+    else:
+        bot.send_message(user_id, f'Папка с именем "{message.text}" уже существует, попробуй другое.')
+
+
+def deleteq(message):
+    user_id = message.from_user.id
+    global state
+
+    if message.text == 'Yes':
+        temp = file_system_data[user_id][current_file[user_id]][0]
+        delete(current_file[user_id], user_id)
+        current_file[user_id] = temp
+        bot.send_message(user_id, 'Папка была удалена')
+    else:
+        bot.send_message(user_id, 'Нет, так нет...')
+
+    state = State.FileManager
+    send_keyboard(message)
+
+
+def rename(message):
+    user_id = message.from_user.id
+    new_name = message.text
+    parent = file_system_data[user_id][current_file[user_id]][0]
+    global state
+
+    if new_name not in file_system_data[user_id]:
+        file_system_data[user_id][parent].remove(current_file[user_id])
+        file_system_data[user_id][parent].append(new_name)
+        for index, item in enumerate(file_system_data[user_id][current_file[user_id]]):
+            if index:
+                file_system_data[user_id][item][0] = new_name
+        file_system_data[user_id][new_name] = file_system_data[user_id].pop(current_file[user_id])
+        bot.send_message(user_id, f'Папка "{current_file[user_id]}" была переименована в "{new_name}"')
+        current_file[user_id] = new_name
+        save()
+        state = State.FileManager
+        send_keyboard(message)
+    else:
+        bot.send_message(user_id, f'Папка с именем "{new_name}" уже существует, попробуй другое.')
+
+
 @bot.message_handler(commands=['start', 'help'])
 def get_text_messages(message):
     global state
@@ -217,101 +316,15 @@ def message_worker(message):
         return 0
 
     if state == State.MainMenu:
-        if message.text == 'Загрузить файл':
-            mode = False
-            if mode:
-                pass
-            else:
-                bot.send_message(user_id, 'Этого не умею, но может скоро научусь.')
-        elif message.text == 'Сохранить файл':
-            mode = False
-            if mode:
-                pass
-            else:
-                bot.send_message(user_id, 'Ой, я так не умею, спроси что-нибудь другое.')
-        elif message.text == 'Искать файл':
-            mode = False
-            if mode:
-                pass
-            else:
-                bot.send_message(user_id, 'Ой, этого я пока не умею.')
-        elif message.text == 'Файловая система':
-            mode = True
-            if mode:
-                state = State.FileManager
-                send_keyboard(message)
-            else:
-                bot.send_message(user_id, 'А что это?')
-
+        main_menu(message)
     elif state == State.FileManager:
-        if message.text == 'delete':
-            if not current_file[user_id] == 'main':
-                state = State.DeleteQ
-                send_keyboard(message)
-            else:
-                bot.send_message(user_id, 'Нельзя удалять папку: "Мой файловый менеджер".')
-        elif message.text == 'rename':
-            if not current_file[user_id] == 'main':
-                state = State.Rename
-                send_keyboard(message)
-            else:
-                bot.send_message(user_id, 'Нельзя переименовывать папку: "Мой файловый менеджер".')
-        elif message.text == 'create':
-            state = State.Edit
-            send_keyboard(message)
-        elif message.text == 'back':
-            if file_system_data[user_id][current_file[user_id]][0] != '':
-                current_file[user_id] = file_system_data[user_id][current_file[user_id]][0]
-                send_keyboard(message)
-            else:
-                bot.send_message(user_id, 'Ты и так уже в самомом начале')
-        elif message.text in file_system_data[user_id]:
-            current_file[user_id] = message.text
-            send_keyboard(message)
-        else:
-            bot.send_message(user_id, 'Такого не знаю попробуй ещё раз')
-            send_keyboard(message)
-
+        file_manager(message)
     elif state == State.Edit:
-        if message.text not in file_system_data[user_id]:
-            file_system_data[user_id][message.text] = [current_file[user_id], ]
-            file_system_data[user_id][current_file[user_id]].append(message.text)
-            current_file[user_id] = message.text
-            bot.send_message(user_id, 'Новая папка создана.')
-            state = State.FileManager
-            send_keyboard(message)
-            save()
-        else:
-            bot.send_message(user_id, f'Папка с именем "{message.text}" уже существует, попробуй другое.')
-
+        edit(message)
     elif state == State.DeleteQ:
-        if message.text == 'Yes':
-            temp = file_system_data[user_id][current_file[user_id]][0]
-            delete(current_file[user_id], user_id)
-            current_file[user_id] = temp
-            bot.send_message(user_id, 'Папка была удалена')
-        else:
-            bot.send_message(user_id, 'Нет, так нет...')
-        state = State.FileManager
-        send_keyboard(message)
-
+        deleteq(message)
     elif state == State.Rename:
-        new_name = message.text
-        father = file_system_data[user_id][current_file[user_id]][0]
-        if new_name not in file_system_data[user_id]:
-            file_system_data[user_id][father].remove(current_file[user_id])
-            file_system_data[user_id][father].append(new_name)
-            for index, item in enumerate(file_system_data[user_id][current_file[user_id]]):
-                if index:
-                    file_system_data[user_id][item][0] = new_name
-            file_system_data[user_id][new_name] = file_system_data[user_id].pop(current_file[user_id])
-            bot.send_message(user_id, f'Папка "{current_file[user_id]}" была переименована в "{new_name}"')
-            current_file[user_id] = new_name
-            state = State.FileManager
-            send_keyboard(message)
-            save()
-        else:
-            bot.send_message(user_id, f'Папка с именем "{new_name}" уже существует, попробуй другое.')
+        rename(message)
 
     print(f'\033[31mState\033[0m: {state.get_str()}\n'
           f'\033[31mUser full\033[0m: {message.from_user}')
